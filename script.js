f00244466b390b0c5b5fe01d596930e6e9acb7be
MickePros <Mikael_Pros@hotmail.com>
2024-10-15 18:25:27 +0200

    Exercise 9 ToDo list in javascript

---
+++ script.js
@@ -0,0 +1,23 @@
+const addNewTask = (text) => {
+    document.querySelector("#todolist").innerHTML += `<li>${text}<i class="icon btn-close"></i></li>`;
+    document.querySelector("#input").value = "";
+}
+
+document.querySelector("#add").addEventListener('click', () => {
+    addNewTask(document.querySelector("#input").value);
+});
+
+document.querySelector("#todo").addEventListener('click', (e) => {
+    if (e.target.tagName == 'I') {
+        e.target.parentNode.remove();
+    }
+    e.target.classList.toggle('done');
+});
+
+"mouseover mouseout".split(" ").forEach( (each) => {
+    document.querySelector("#todo").addEventListener(each, (e) => {
+        if (e.target.tagName == 'I') {
+            e.target.parentNode.classList.toggle('remove');
+        }
+    });
+});
